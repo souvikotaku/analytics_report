4a7e3d3ac7a6f999c2ea60d2f690c2f56c4e1a5e
souvikotaku <manson1989@>
2025-01-30 14:48:42 +0530

    decoded url components

--- src/widgets/layout/dashboard-navbar.jsx
+++ src/widgets/layout/dashboard-navbar.jsx
@@ -97,7 +97,7 @@ export function DashboardNavbar() {
                 pointerEvents: 'none',
               }}
             >
-              {inner}
+              {decodeURIComponent(inner)}
             </Typography>
               )
             }
